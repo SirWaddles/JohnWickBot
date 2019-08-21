a1aa60ebb7604890d4a2987d6b2d35189fdeb0c4
Waddlesworth <github@genj.io>
2019-08-21 14:41:58 +1000

    use a channel

--- src/client.rs
+++ src/client.rs
@@ -1,16 +1,10 @@
 use std::str;
 use std::time::{Duration, Instant};
 use futures::{Future, Async};
+use futures::sync::mpsc;
 use tokio::net::{TcpStream, tcp::ConnectFuture};
 use tokio::timer::{Delay, Error as TimerError};
 use tokio::prelude::AsyncRead;
-
-fn process_data(data: Vec<u8>, len: usize) {
-    match str::from_utf8(&data[0..len]) {
-        Ok(v) => println!("data: {} end", v),
-        Err(_) => return,
-    }
-}
 
 pub struct ClientError;
 
@@ -26,6 +20,11 @@ impl From<TimerError> for ClientError {
     }
 }
 
+pub struct RequestState {
+    pub channel_id: u64,
+    pub message_content: String,
+}
+
 enum ClientFutureState {
     Connecting(ConnectFuture),
     Reading(TcpStream),
@@ -35,17 +34,32 @@ enum ClientFutureState {
 
 pub struct ClientFuture {
     state: ClientFutureState,
+    sender: mpsc::UnboundedSender<RequestState>,
 }
 
 impl ClientFuture {
-    pub fn new() -> Self {
+    pub fn new(sender: mpsc::UnboundedSender<RequestState>) -> Self {
         Self {
             state: ClientFutureState::Connecting(Self::connect()),
+            sender,
         }
     }
 
     fn connect() -> ConnectFuture {
         TcpStream::connect(&"127.0.0.1:27020".parse().unwrap())
+    }
+
+    fn process_data(&self, data: Vec<u8>, len: usize) {
+        match str::from_utf8(&data[0..len]) {
+            Ok(v) => {
+                println!("Data: {}", v);
+                self.sender.unbounded_send(RequestState {
+                    message_content: "Hi there!".to_owned(),
+                    channel_id: 613583885699383307,
+                }).unwrap();
+            },
+            Err(_) => return,
+        }
     }
 }
 
@@ -54,54 +68,58 @@ impl Future for ClientFuture {
     type Error = ClientError;
 
     fn poll(&mut self) -> Result<Async<Self::Item>, Self::Error> {
-        match &mut self.state {
-            ClientFutureState::Disconnected => {
-                let when = Instant::now() + Duration::from_secs(30);
-                let mut delay = Delay::new(when);
-                delay.poll()?;
-                self.state = ClientFutureState::Waiting(delay);
-            },
-            ClientFutureState::Waiting(delay) => {
-                match delay.poll() {
-                    Ok(Async::Ready(_)) => {
-                        let mut connect = Self::connect();
-                        connect.poll()?;
-                        self.state = ClientFutureState::Connecting(connect);
-                    },
-                    Ok(Async::NotReady) => (),
-                    Err(_) => {
-                        return Err(ClientError);
+        loop {
+            match &mut self.state {
+                ClientFutureState::Disconnected => {
+                    println!("disconnected");
+                    let when = Instant::now() + Duration::from_secs(30);
+                    let mut delay = Delay::new(when);
+                    delay.poll()?;
+                    self.state = ClientFutureState::Waiting(delay);
+                    break
+                },
+                ClientFutureState::Waiting(delay) => {
+                    println!("waiting");
+                    match delay.poll() {
+                        Ok(Async::Ready(_)) => {
+                            let mut connect = Self::connect();
+                            connect.poll()?;
+                            self.state = ClientFutureState::Connecting(connect);
+                            break
+                        },
+                        Ok(Async::NotReady) => break,
+                        Err(_) => {
+                            return Err(ClientError);
+                        }
                     }
-                }
-            },
-            ClientFutureState::Connecting(connect) => {
-                match connect.poll() {
-                    Ok(Async::Ready(stream)) => {
-                        self.state = ClientFutureState::Reading(stream);
-                        self.poll()?;
-                    },
-                    Ok(Async::NotReady) => (),
-                    Err(_) => {
-                        self.state = ClientFutureState::Disconnected;
-                        self.poll()?;
+                },
+                ClientFutureState::Connecting(connect) => {
+                    println!("connecting");
+                    match connect.poll() {
+                        Ok(Async::Ready(stream)) => {
+                            self.state = ClientFutureState::Reading(stream);
+                        },
+                        Ok(Async::NotReady) => break,
+                        Err(_) => {
+                            self.state = ClientFutureState::Disconnected;
+                        }
                     }
-                }
-            },
-            ClientFutureState::Reading(stream) => {
-                let mut data = vec![0u8; 256];
-                match stream.poll_read(&mut data) {
-                    Ok(Async::Ready(bytes)) => {
-                        process_data(data, bytes);
-                        self.poll()?;
-                    },
-                    Ok(Async::NotReady) => (),
-                    Err(_) => {
-                        self.state = ClientFutureState::Disconnected;
-                        self.poll()?;
-                    },
-                };
-            },
-        };
+                },
+                ClientFutureState::Reading(stream) => {
+                    println!("reading");
+                    let mut data = vec![0u8; 256];
+                    match stream.poll_read(&mut data) {
+                        Ok(Async::Ready(bytes)) => {
+                            self.process_data(data, bytes);
+                        },
+                        Ok(Async::NotReady) => break,
+                        Err(_) => {
+                            self.state = ClientFutureState::Disconnected;
+                        },
+                    };
+                },
+            };
+        }
         Ok(Async::NotReady)
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -8,10 +8,9 @@ extern crate serde_json;
 mod client;
 mod db;
 
-use std::sync::{Mutex, Arc};
-use std::{thread, time};
+use std::thread;
 use serenity::Client;
-use serenity::prelude::{TypeMapKey, EventHandler, Context};
+use serenity::prelude::{EventHandler, Context};
 use serenity::model::channel::Message;
 use serde_json::json;
 use hyper::rt;
@@ -19,6 +18,7 @@ use hyper::http::Request;
 use futures::{Async, Stream};
 use futures::future::Future;
 use futures::sync::mpsc;
+use client::RequestState;
 
 type HyperClient = hyper::Client<hyper_tls::HttpsConnector<hyper::client::HttpConnector>>;
 
@@ -27,45 +27,15 @@ struct JWHandler;
 impl EventHandler for JWHandler {
     fn message(&self, ctx: Context, msg: Message) {
         if msg.content == "!ping" {
-            let mut data = ctx.data.write();
-            let hyper_state = data.get_mut::<HyperState>().unwrap();
-            let mut req_state = hyper_state.lock().unwrap();
-            req_state.requests.push(RequestState {
-                channel_id: msg.channel_id.0,
-                message_content: "Pong!".to_owned(),
-            });
-        }
-    }
-}
-
-struct HyperState;
-
-impl TypeMapKey for HyperState {
-    type Value = Arc<Mutex<RequestQueue>>;
-}
-
-struct RequestState {
-    channel_id: u64,
-    message_content: String,
-}
-
-struct RequestQueue {
-    requests: Vec<RequestState>,
-}
-
-impl RequestQueue {
-    fn new() -> Self {
-        Self {
-            requests: Vec::new(),
+            
         }
     }
 }
 
 struct HyperRuntime {
-    requests: Arc<Mutex<RequestQueue>>,
     client: HyperClient,
     bot_token: String,
-    receiver: mpsc::UnboundedReceiver,
+    receiver: mpsc::UnboundedReceiver<RequestState>,
 }
 
 impl HyperRuntime {
@@ -106,21 +76,14 @@ impl Future for HyperRuntime {
 
     fn poll(&mut self) -> Result<Async<Self::Item>, Self::Error> {
         loop {
-            let to_run = {
-                let mut requests_queue = self.requests.lock().unwrap();
-                let mut data = Vec::new();
-                data.append(&mut requests_queue.requests);
-
-                data
+            let request = match self.receiver.poll() {
+                Ok(Async::Ready(Some(val))) => val,
+                Ok(Async::Ready(None)) => return Ok(Async::Ready(())),
+                Ok(Async::NotReady) => break,
+                Err(e) => return Err(e),
             };
 
-            println!("Requests to run: {}", to_run.len());
-
-            for request in to_run {
-                rt::spawn(self.send_message(request));
-            }
-
-            thread::sleep(time::Duration::from_millis(2000));
+            rt::spawn(self.send_message(request));
         }
         Ok(Async::NotReady)
     }
@@ -128,27 +91,19 @@ impl Future for HyperRuntime {
 
 fn main() {
     let bot_token = std::env::var("DISCORD_TOKEN").unwrap();
-    let request_state = Arc::new(Mutex::new(RequestQueue::new()));
     let mut discord_client = Client::new(bot_token.clone(), JWHandler).unwrap();
-    
-    {
-        let mut data = discord_client.data.write();
-        data.insert::<HyperState>(request_state.clone());
-    }
-
     let https = hyper_tls::HttpsConnector::new(4).unwrap();
     let http_client = hyper::Client::builder().build::<_, hyper::Body>(https);
 
-    let unb_channel = mspc::unbounded<RequestQueue>();
+    let unb_channel = mpsc::unbounded::<RequestState>();
 
     let hyper_runtime = HyperRuntime {
-        requests: request_state,
         client: http_client,
         bot_token,
         receiver: unb_channel.1,
     };
 
-    let client_runtime = client::ClientFuture::new().map_err(|_| println!("Client Error"));
+    let client_runtime = client::ClientFuture::new(unb_channel.0).map_err(|_| println!("Client Error"));
 
     thread::spawn(move || {
         rt::run(client_runtime.join(hyper_runtime).and_then(|_| {
